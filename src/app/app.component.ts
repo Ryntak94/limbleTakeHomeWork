8815d0b5bed3d0d542de3fe78a47c11df2d3aa23
ryntak94 <ryan.matthews.professional@gmail.com>
2022-08-10 08:06:21 -0500

    preserve whitespace after bold tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,16 +29,16 @@ export class AppComponent implements OnInit {
   tagIndices: number[] = []
   users: User[] = [
     {id: 1, name: 'Kevin', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 2, name: 'Jeff', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 3, name: 'Bryan', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 4, name: 'Gabbey', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 5, name: 'Kerry', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 6, name: 'Kaleia', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 7, name: 'Ryan', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 8, name: 'Ian', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 9, name: 'Adam', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 10, name: 'James Madison', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'},
-    {id: 10, name: 'Alexander Hamilton', imgSrc: 'https://www.biography.com/.image/t_share/MTQzMzA3MjQ0MzI5NTEwNDcx/kevin-hart_gettyimages-450909048jpg.jpg'}
+    {id: 2, name: 'Jeff', imgSrc: 'https://pyxis.nymag.com/v1/imgs/187/9d6/3d89a91cdd64dabcd5908c5a6a6bc0a2c6-11-jeff-goldblum.rsquare.w700.jpg'},
+    {id: 3, name: 'Bryan', imgSrc: 'https://hips.hearstapps.com/esquireuk.cdnds.net/15/37/2048x2730/2048x2730-walter-white-rumour-bryan-cranston-43-jpg-21006810.jpg'},
+    {id: 4, name: 'Gabbey', imgSrc: 'https://imgix.ranker.com/user_node_img/3177/63527153/original/gabrielle-douglas-athletes-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&crop=faces&h=125&w=125'},
+    {id: 5, name: 'Kerry', imgSrc: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTQ0NzQxMzI0MjQ1OTY3OTE4/kerry_washington_jason_laveris_filmmagic_getty_images_633021804_profile.jpg'},
+    {id: 6, name: 'Kaleia', imgSrc: 'https://m.media-amazon.com/images/M/MV5BOWNhZjc5MDQtZjA1My00M2M5LTk2NzUtN2I3NjUyODA0ZDc5XkEyXkFqcGdeQXVyODEzNDE0MjM@._V1_UX180_CR0,0,180,180_AL_.jpg'},
+    {id: 7, name: 'Ryan', imgSrc: 'https://www.themoviedb.org/t/p/w235_and_h235_face/4SYTH5FdB0dAORV98Nwg3llgVnY.jpg'},
+    {id: 8, name: 'Ian', imgSrc: 'https://i.guim.co.uk/img/media/d56cf414fd7f4681a7fc2b54c367ef6f4364a4c2/0_288_3567_3564/master/3567.jpg?width=1020&quality=85&fit=max&s=13d41921a7e7ebd4f3edd875ae69e2a4'},
+    {id: 9, name: 'Adam', imgSrc: 'https://pittsburghlectures.org/wp-content/uploads/2019/02/adam-savage.jpg'},
+    {id: 10, name: 'James Madison', imgSrc: 'https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDUyOTAxOTA0MDAwNjQ2/james-madison-biography-fourth-president-of-the-united-states.jpg'},
+    {id: 10, name: 'Alexander Hamilton', imgSrc: 'https://i.guim.co.uk/img/media/611c74f4533083450171c9b974b41eacdc9ccf30/344_198_2324_1394/master/2324.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=25204c640b588165b4d5e63f711e623d'}
   ]
   userTrie: Trie = new Trie(this.users);
   filteredUsers: User[] = this.users;
@@ -155,7 +155,7 @@ export class AppComponent implements OnInit {
     let formattedComment = this.comment
     this.comment = "";
     for(let i = this.taggedUsers.length - 1; i >= 0; i--) {
-      formattedComment = formattedComment.slice(0, this.tagIndices[i]) + formattedComment.slice(this.tagIndices[i]).replace(this.taggedUsers[i].name, `<b>${this.taggedUsers[i].name}</b>`)
+      formattedComment = formattedComment.slice(0, this.tagIndices[i]) + formattedComment.slice(this.tagIndices[i]).replace(`@${this.taggedUsers[i].name}`, `<b>@${this.taggedUsers[i].name}</b>`).replace('</b> ', '</b>&nbsp')
     }
     console.log(formattedComment)
     this.comments.push(formattedComment);
