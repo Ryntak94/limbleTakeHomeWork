b0b16ebe1b4d0a71a10df048995dfcabcaa60b25
ryntak94 <ryan.matthews.professional@gmail.com>
2022-08-11 20:49:09 -0500

    more styling, fix more bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,7 +44,7 @@ export class AppComponent implements OnInit {
     {id: 8, name: 'Ian', imgSrc: 'https://i.guim.co.uk/img/media/d56cf414fd7f4681a7fc2b54c367ef6f4364a4c2/0_288_3567_3564/master/3567.jpg?width=1020&quality=85&fit=max&s=13d41921a7e7ebd4f3edd875ae69e2a4'},
     {id: 9, name: 'Adam', imgSrc: 'https://pittsburghlectures.org/wp-content/uploads/2019/02/adam-savage.jpg'},
     {id: 10, name: 'James Madison', imgSrc: 'https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDUyOTAxOTA0MDAwNjQ2/james-madison-biography-fourth-president-of-the-united-states.jpg'},
-    {id: 10, name: 'Alexander Hamilton', imgSrc: 'https://i.guim.co.uk/img/media/611c74f4533083450171c9b974b41eacdc9ccf30/344_198_2324_1394/master/2324.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=25204c640b588165b4d5e63f711e623d'}
+    {id: 11, name: 'Alexander Hamilton', imgSrc: 'https://i.guim.co.uk/img/media/611c74f4533083450171c9b974b41eacdc9ccf30/344_198_2324_1394/master/2324.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=25204c640b588165b4d5e63f711e623d'}
   ]
   userTrie: Trie = new Trie(this.users);
   filteredUsers: User[] = this.users;
@@ -57,7 +57,7 @@ export class AppComponent implements OnInit {
 
     this.commentService.userSelected.subscribe(user =>  {
       if(user) this.finalizeTag(user);
-      this.userListEnabled = false;
+      this.closeUserList();
       this.commentBox?.nativeElement.focus();
       if(!this.taggedUsers.has(user)) this.taggedUsers.add(user)
       this.tagIndices.push(this.tagIdx!)
@@ -90,7 +90,7 @@ export class AppComponent implements OnInit {
     })
 
     this.commentService.emitCancelTag.subscribe(event =>  {
-      this.userListEnabled = false;
+      this.closeUserList();
       this.commentBox?.nativeElement.focus();
       
       this.comment = this.comment.slice(0, this.tagIdx! + this.tag.length ) + " " + this.comment.slice(this.tagIdx! + this.tag.length)
@@ -112,7 +112,7 @@ export class AppComponent implements OnInit {
     }
 
     if(indices.length > 0) tagging = this.selectCurrentTagOrNull(caretPosition, indices, event);
-    if(!tagging) this.userListEnabled = false;
+    if(!tagging) this.closeUserList();
   }
 
   getIndices(tags: string[])  {
@@ -146,12 +146,10 @@ export class AppComponent implements OnInit {
 
   refocusOnClick(event: any) {
     if(this.userListEnabled) {
-      this.userListEnabled = false;
+      this.closeUserList();
       this.commentBox?.nativeElement.focus()
     }
-    if(this.comment.length !== event.target.selectionStart) {
       this.checkForTaggingUser(event);
-    }
 
   }
 
@@ -166,7 +164,7 @@ export class AppComponent implements OnInit {
     }
     formattedComment = formattedComment.replaceAll(' ', '&nbsp;')
     this.comments.push(formattedComment);
-    this.userListEnabled = false;
+    this.closeUserList();
     for(const user of taggedUsersArr) {
       if(user !== undefined && user !== null)  {
         alert(user.name + ", " + user.id)
@@ -181,6 +179,10 @@ export class AppComponent implements OnInit {
 
   finalizeTag(user: User) {
     this.updateTag(`@${user!.name}`)
+  }
+
+  closeUserList() {
+    this.userListEnabled = false;
   }
 
   title = 'tagger';
